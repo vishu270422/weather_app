2e692c4ad7ae65c8758849aabad161b93b5c4dc4
vishu270422 <129824992+vishu270422@users.noreply.github.com>
2023-12-20 21:09:43 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Weather from './Components/Weather.jsx';
+
+function App() {
+  return (
+    <div className="App">
+      <Weather/>
+ 
+    </div>
+  );
+}
+
+export default App;
